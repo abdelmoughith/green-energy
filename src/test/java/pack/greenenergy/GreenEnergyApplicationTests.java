bec8a642452732a0c9e383e3b6b5b73461d326f2
MOUGHIT <abdelmoughith2002@gmail.com>
2025-11-24 17:49:28 +0100

    green-energy started

---
+++ src/test/java/pack/greenenergy/GreenEnergyApplicationTests.java
@@ -0,0 +1,13 @@
+package pack.greenenergy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenEnergyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
